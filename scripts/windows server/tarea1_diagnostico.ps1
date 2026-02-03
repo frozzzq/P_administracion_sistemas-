1c809d825322b031dbcfdcaa105d642f0d11fae2
josue vega <veggjoss@gmail.com>
2026-02-03 00:02:37 -0600

    script del segundo nodo (windows server)

--- scripts/windows server/tarea1_diagnostico.ps1
+++ scripts/windows server/tarea1_diagnostico.ps1
@@ -1,1 +1,9 @@
+write-host '===================================================================' -foregroundcolor cyan
+write-host '====================DIAGNOSTICO DE WINDOWS SERVER.=================' -foregroundcolor blue
+write-host '===================================================================' -foregroundcolor cyan
+write-host "nombre del equipo	: $env:COMPUTERNAME"
+write-host "fecha y hora	: $(get-date)"
+write-host '===================================================================' -foregroundcolor cyan
+
+
 
