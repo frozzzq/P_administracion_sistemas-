5646de586bff8fcb954bfb145ae24054837e458b
frozzzq <veggjoss@gmail.com>
2026-02-04 20:13:36 +0100

    script de powershell completado: (hostname, fecha, ip, espacio)

--- scripts/windows server/tarea1_diagnostico.ps1
+++ scripts/windows server/tarea1_diagnostico.ps1
@@ -3,6 +3,15 @@ write-host '====================DIAGNOSTICO DE WINDOWS SERVER.================='
 write-host '===================================================================' -foregroundcolor cyan
 write-host "nombre del equipo	: $env:COMPUTERNAME"
 write-host "fecha y hora	: $(get-date)"
+
+$ip = get-netipaddress -addressfamily ipv4 | where-object { $_.interfacealias -like "*Ethernet 2*" -and $_.IPAddress -notlike "169.254*"} | select-object -first 1
+write-host "IP actual		" -nonewline; write-host $ip.ipaddress -foregroundcolor yellow
+
+$diskC = get-ciminstance win32_logicaldisk | where-object { $_.deviceid -eq "C:" }
+$free = [math]::round($diskC.FreeSpace / 1GB, 2)
+$total = [math]::round($diskC.size /1GB, 2)
+write-host "espacio en disco:  " -nonewline; write-host "$free GB disponibles de $total GB" -foregroundcolor yellow
+
 write-host '===================================================================' -foregroundcolor cyan
 
 
