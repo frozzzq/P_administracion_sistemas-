458283ed9b3acb317dc0ebb3e8ba1a2005686bbe
josue vega <veggjoss@gmail.com>
2026-02-17 12:56:17 -0600

    inicio de menu

--- scripts/windows server/tarea3_diagnostico.ps1
+++ scripts/windows server/tarea3_diagnostico.ps1
@@ -1,1 +1,32 @@
+Write Host "======================================================================" -ForegroundColor Yellow
+Write Host "                           SERVIDOR DNS                               "
+Write Host "======================================================================" -ForegroundColor Yellow
+
+
+do{
+    write host "[1] - VERIFICAR INSTALACION DNS"
+    write host "[2] - INSTALAR SERVICIO DNS"
+    write host "[3] - REMOVER SERVICIO DNS"
+
+    $opc = Read host "ingrese una opcion:"  
+
+    switch{
+
+        case "1":
+            get-WindowsFeature DNS
+
+            
+    
+    
+    
+    }
+
+
+
+
+}while($opc -eq "si")
+
+
+
+
 get-WindowsFeature DNS
