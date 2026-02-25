9c8edd002c58090a147143c0e2947da3e5bb6586
josue vega <veggjoss@gmail.com>
2026-02-25 01:04:22 -0600

    nuevos archivos1

--- scripts/windows server/tarea4_powershell/main_P4.ps1
+++ scripts/windows server/tarea4_powershell/main_P4.ps1
@@ -1,31 +1,28 @@
-
 . "$PSScriptRoot\lib\funcion_ssh.ps1"
-. "$PSScriptRoot\lib\funcion_dhcp.ps1"   
-. "$PSScriptRoot\lib\funcion_dns.ps1"    
+. "$PSScriptRoot\lib\funcion_dhcp.ps1"
+. "$PSScriptRoot\lib\funcion_dns.ps1"
 
 function menu {
-    Write-Host "`n===================================================================" -ForegroundColor Cyan
-    Write-Host "             ADMINISTRACIÓN DE WINDOWS SERVER                      " -ForegroundColor Blue
     Write-Host "===================================================================" -ForegroundColor Cyan
-    Write-Host "1. Gestionar SSH"       -ForegroundColor Yellow
-    Write-Host "2. Gestionar DHCP"      -ForegroundColor Yellow
-    Write-Host "3. Gestionar DNS"       -ForegroundColor Yellow
-    Write-Host "4. Salir"               -ForegroundColor Yellow
+    Write-Host "          ADMINISTRACION DE WINDOWS SERVER                         " -ForegroundColor Blue
+    Write-Host "===================================================================" -ForegroundColor Cyan
+    Write-Host "1. Gestionar SSH"  -ForegroundColor Yellow
+    Write-Host "2. Gestionar DHCP" -ForegroundColor Yellow
+    Write-Host "3. Gestionar DNS"  -ForegroundColor Yellow
+    Write-Host "4. Salir"          -ForegroundColor Yellow
 }
 
 do {
     menu
-    $opcion = Read-Host "`nElige una opción"
-
+    $opcion = Read-Host "Elige una opcion"
     switch ($opcion) {
         "1" { menuSsh }
-        "2" { menuDhcp}
+        "2" { menuDhcp }
         "3" { menuDns }
-        default { Write-Host "Opción inválida." -ForegroundColor Red }
+        "4" { Write-Host "Saliendo..." -ForegroundColor Cyan }
+        default { Write-Host "Opcion invalida." -ForegroundColor Red }
     }
-
     if ($opcion -ne "4") {
-        $continuar = Read-Host "`n¿Volver al menú? (si/no)"
+        $continuar = Read-Host "Volver al menu? (si/no)"
     }
-
 } while ($opcion -ne "4" -and $continuar -eq "si")
